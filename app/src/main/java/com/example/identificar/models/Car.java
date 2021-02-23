8aa89dc222a8757927f2b2f7ae58b5b05f4a164a
Dilan Jayamanne <dicmandilan@gmail.com>
2021-02-23 20:58:24 +0530

    project identificar added and created the first activity

---
+++ app/src/main/java/com/example/identificar/models/Car.java
@@ -0,0 +1,5 @@
+package com.example.identificar.models;
+
+public class Car extends Vehicle{
+    
+}
---
+++ app/src/main/java/com/example/identificar/models/Vehicle.java
@@ -0,0 +1,5 @@
+package com.example.identificar.models;
+
+public abstract class Vehicle {
+
+}
