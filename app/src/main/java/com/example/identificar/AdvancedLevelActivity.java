062dcfbdfe7103fd36245352d4bdf2a9d51c35d4
Dilan Jayamanne <dicmandilan@gmail.com>
2021-03-12 21:39:19 +0530

    switch timer functionality for the adavanced level activity added

--- app/src/main/java/com/example/identificar/AdvancedLevelActivity.java
+++ app/src/main/java/com/example/identificar/AdvancedLevelActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,7 +47,7 @@ public class AdvancedLevelActivity extends AppCompatActivity {
     CountDown countTimer;
     TextView timerTextView;
 
-    int milliSec = 20000;
+    int milliSec = 10000;
     int countDownInterval = 1000;
 
     @Override
@@ -97,8 +99,34 @@ public class AdvancedLevelActivity extends AppCompatActivity {
         correct.setText(null);
         score.setText("0");
 
-        countTimer.initializeCountDown(switchChecked, timerTextView, btnSubmit, this);
+        countTimer.initializeCountDown(switchChecked, timerTextView, this);
         countTimer.startCount();
+
+        timerTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(timerTextView.getText().toString().equalsIgnoreCase("TimesUp!")){
+                    if(btnSubmit.getText().toString().equalsIgnoreCase("Submit")) {
+                        submitFunctionality();
+                        countTimer.stopCount();
+                        countTimer.initializeCountDown(switchChecked, timerTextView, getApplicationContext());
+                        countTimer.startCount();
+                    }else if(btnSubmit.getText().toString().equalsIgnoreCase("Next")){
+                        nextFunctionality();
+                    }
+                }
+            }
+        });
     }
 
     public void generateRandomResources(){
@@ -130,143 +158,163 @@ public class AdvancedLevelActivity extends AppCompatActivity {
     public void onAdvancedSubmitPressed(View view) {
 
         if(btnSubmit.getText().toString().equalsIgnoreCase("submit")) {
-            if (!car1EditText.getText().toString().isEmpty() ||
-                    !car2EditText.getText().toString().isEmpty() ||
-                    !car3EditText.getText().toString().isEmpty()
-            ) {
 
-                // checking if all the car names given are correct
-                if (compareCarNames(car1EditText, cars.getMakes()[randomInt1]) &&
-                        compareCarNames(car2EditText, cars.getMakes()[randomInt2]) &&
-                        compareCarNames(car3EditText, cars.getMakes()[randomInt3])) {
-
-                    // is enabled are checked to execute the score by one when user re-corrects without exceeding the chances
-                    if(car1EditText.isEnabled() || car2EditText.isEnabled() || car3EditText.isEnabled()) {
-                        if(car1EditText.isEnabled()){
-                            scoreValue ++;
-                            score.setText(Integer.toString(scoreValue));
-                        }
-                        if(car2EditText.isEnabled()){
-                            scoreValue ++;
-                            score.setText(Integer.toString(scoreValue));
-                        }
-                        if(car3EditText.isEnabled()){
-                            scoreValue ++;
-                            score.setText(Integer.toString(scoreValue));
-                        }
-
-                    }
-                    correct.setText(R.string.correct);
-                    correct.setTextColor(getResources().getColor(R.color.identificarGreen));
-
-                    //setting edit text views green custhey are correct
-                    car1EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
-                    car2EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
-                    car3EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
-
-                    //making edittext views uneditable
-                    car1EditText.setEnabled(false);
-                    car2EditText.setEnabled(false);
-                    car3EditText.setEnabled(false);
-
-                    btnSubmit.setText(R.string.btn_next);
-                }else{
-                    // if some car names are not correct.
-
-                    currentIncorrectGuess --;
-                    correct.setText("left: " + Integer.toString(currentIncorrectGuess - 1));
-                    correct.setTextColor(getResources().getColor(R.color.identificarRed));
-
-                    if(!compareCarNames(car1EditText, cars.getMakes()[randomInt1])){
-                        car1EditText.setTextColor(getResources().getColor(R.color.identificarRed));
-                    }else{
-                        car1EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
-
-                        //score adds only when user typed the on the first check
-                        if(car1EditText.isEnabled()){
-                            scoreValue ++;
-                            score.setText(Integer.toString(scoreValue));
-                        }
-                        car1EditText.setEnabled(false);
-
-                    }
-                    if(!compareCarNames(car2EditText, cars.getMakes()[randomInt2])){
-                        car2EditText.setTextColor(getResources().getColor(R.color.identificarRed));
-                    }else{
-                        car2EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
-                        if(car2EditText.isEnabled()){
-                            scoreValue ++;
-                            score.setText(Integer.toString(scoreValue));
-                        }
-                        car2EditText.setEnabled(false);
-                    }
-                    if(!compareCarNames(car3EditText, cars.getMakes()[randomInt3])){
-                        car3EditText.setTextColor(getResources().getColor(R.color.identificarRed));
-                    }else{
-                        car3EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
-                        if(car3EditText.isEnabled()){
-                            scoreValue ++;
-                            score.setText(Integer.toString(scoreValue));
-                        }
-                        car3EditText.setEnabled(false);
-
-                    }
-
-                    if(currentIncorrectGuess == 0) {
-                        correct.setText(null);
-                        if(!compareCarNames(car1EditText, cars.getMakes()[randomInt1]))
-                            wrong1.setText(cars.getMakes()[randomInt1]);
-                        if(!compareCarNames(car2EditText, cars.getMakes()[randomInt2]))
-                            wrong2.setText(cars.getMakes()[randomInt2]);
-                        if(!compareCarNames(car3EditText, cars.getMakes()[randomInt3]))
-                            wrong3.setText(cars.getMakes()[randomInt3]);
-
-                        correct.setTextColor(getResources().getColor(R.color.identificarRed));
-                        correct.setText(R.string.wrong);
-                        btnSubmit.setText(R.string.btn_next);
-                    }
-                }
-
-            } else {
-                Toast.makeText(this, "Please fill all the text boxes", Toast.LENGTH_SHORT).show();
-            }
-
-
+            submitFunctionality();
 
         }else if(btnSubmit.getText().toString().equalsIgnoreCase("next")){
 
-            countTimer.stopCount();
-
-            car1EditText.setTextColor(getResources().getColor(R.color.identificarPrimary));
-            car2EditText.setTextColor(getResources().getColor(R.color.identificarPrimary));
-            car3EditText.setTextColor(getResources().getColor(R.color.identificarPrimary));
-            car1EditText.setText(null);
-            car2EditText.setText(null);
-            car3EditText.setText(null);
-            car1EditText.setEnabled(true);
-            car2EditText.setEnabled(true);
-            car3EditText.setEnabled(true);
-            correct.setText(null);
-            wrong1.setText(null);
-            wrong2.setText(null);
-            wrong3.setText(null);
-            //scoreValue = 0;
-            currentIncorrectGuess = 4;
-
-            generateRandomResources();
-            btnSubmit.setText(R.string.btn_submit);
-
-            countTimer.initializeCountDown(switchChecked, timerTextView, btnSubmit, this);
-            countTimer.startCount();
+            nextFunctionality();
         }
 
         // TODO: ignore the spaces when getting user inputs
         //TODO: implement the count down for all the attempts
     }
 
+    public void submitFunctionality(){
+        if (!car1EditText.getText().toString().isEmpty() ||
+                !car2EditText.getText().toString().isEmpty() ||
+                !car3EditText.getText().toString().isEmpty()
+        ) {
+
+            // checking if all the car names given are correct
+            if (compareCarNames(car1EditText, cars.getMakes()[randomInt1]) &&
+                    compareCarNames(car2EditText, cars.getMakes()[randomInt2]) &&
+                    compareCarNames(car3EditText, cars.getMakes()[randomInt3])) {
+
+                // is enabled are checked to execute the score by one when user re-corrects without exceeding the chances
+                if(car1EditText.isEnabled() || car2EditText.isEnabled() || car3EditText.isEnabled()) {
+                    if(car1EditText.isEnabled()){
+                        scoreValue ++;
+                        score.setText(Integer.toString(scoreValue));
+                    }
+                    if(car2EditText.isEnabled()){
+                        scoreValue ++;
+                        score.setText(Integer.toString(scoreValue));
+                    }
+                    if(car3EditText.isEnabled()){
+                        scoreValue ++;
+                        score.setText(Integer.toString(scoreValue));
+                    }
+
+                }
+                correct.setText(R.string.correct);
+                correct.setTextColor(getResources().getColor(R.color.identificarGreen));
+
+                //setting edit text views green custhey are correct
+                car1EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
+                car2EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
+                car3EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
+
+                //making edittext views uneditable
+                car1EditText.setEnabled(false);
+                car2EditText.setEnabled(false);
+                car3EditText.setEnabled(false);
+
+                btnSubmit.setText(R.string.btn_next);
+            }else{
+                // if some car names are not correct.
+
+                currentIncorrectGuess --;
+                correct.setText("left: " + Integer.toString(currentIncorrectGuess - 1));
+                correct.setTextColor(getResources().getColor(R.color.identificarRed));
+
+                if(!compareCarNames(car1EditText, cars.getMakes()[randomInt1])){
+                    car1EditText.setTextColor(getResources().getColor(R.color.identificarRed));
+                }else{
+                    car1EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
+
+                    //score adds only when user typed the on the first check
+                    if(car1EditText.isEnabled()){
+                        scoreValue ++;
+                        score.setText(Integer.toString(scoreValue));
+                    }
+                    car1EditText.setEnabled(false);
+
+                }
+                if(!compareCarNames(car2EditText, cars.getMakes()[randomInt2])){
+                    car2EditText.setTextColor(getResources().getColor(R.color.identificarRed));
+                }else{
+                    car2EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
+                    if(car2EditText.isEnabled()){
+                        scoreValue ++;
+                        score.setText(Integer.toString(scoreValue));
+                    }
+                    car2EditText.setEnabled(false);
+                }
+                if(!compareCarNames(car3EditText, cars.getMakes()[randomInt3])){
+                    car3EditText.setTextColor(getResources().getColor(R.color.identificarRed));
+                }else{
+                    car3EditText.setTextColor(getResources().getColor(R.color.identificarGreen));
+                    if(car3EditText.isEnabled()){
+                        scoreValue ++;
+                        score.setText(Integer.toString(scoreValue));
+                    }
+                    car3EditText.setEnabled(false);
+
+                }
+
+                onZeroAttemptsLeft();
+            }
+
+        } else {
+            Toast.makeText(this, "Please fill all the text boxes", Toast.LENGTH_SHORT).show();
+            currentIncorrectGuess --;
+            correct.setText("left: " + Integer.toString(currentIncorrectGuess - 1));
+            correct.setTextColor(getResources().getColor(R.color.identificarRed));
+
+            onZeroAttemptsLeft();
+
+            //TODO: have to implement the functionality when current timer becomes 0
+        }
+    }
+
+    public void nextFunctionality(){
+        countTimer.stopCount();
+
+        car1EditText.setTextColor(getResources().getColor(R.color.identificarPrimary));
+        car2EditText.setTextColor(getResources().getColor(R.color.identificarPrimary));
+        car3EditText.setTextColor(getResources().getColor(R.color.identificarPrimary));
+        car1EditText.setText(null);
+        car2EditText.setText(null);
+        car3EditText.setText(null);
+        car1EditText.setEnabled(true);
+        car2EditText.setEnabled(true);
+        car3EditText.setEnabled(true);
+        correct.setText(null);
+        wrong1.setText(null);
+        wrong2.setText(null);
+        wrong3.setText(null);
+        //scoreValue = 0;
+        currentIncorrectGuess = 4;
+
+        generateRandomResources();
+        btnSubmit.setText(R.string.btn_submit);
+
+        countTimer.initializeCountDown(switchChecked, timerTextView, this);
+        countTimer.startCount();
+    }
+
+
     public boolean compareCarNames(EditText e1, String carName){
         return e1.getText().toString().equalsIgnoreCase(carName);
     }
+
+    public void onZeroAttemptsLeft(){
+        if(currentIncorrectGuess == 0) {
+            correct.setText(null);
+            if(!compareCarNames(car1EditText, cars.getMakes()[randomInt1]))
+                wrong1.setText(cars.getMakes()[randomInt1]);
+            if(!compareCarNames(car2EditText, cars.getMakes()[randomInt2]))
+                wrong2.setText(cars.getMakes()[randomInt2]);
+            if(!compareCarNames(car3EditText, cars.getMakes()[randomInt3]))
+                wrong3.setText(cars.getMakes()[randomInt3]);
+
+            correct.setTextColor(getResources().getColor(R.color.identificarRed));
+            correct.setText(R.string.wrong);
+            btnSubmit.setText(R.string.btn_next);
+        }
+    }
+
 
     @Override
     protected void onStop() {
